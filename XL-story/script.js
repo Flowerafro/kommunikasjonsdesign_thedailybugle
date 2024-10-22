95d0d02bf4e4dbbf06f8e7ea53e2ee03078d573f
Flowerafro <linehenriksen0@gmail.com>
2024-10-22 16:18:21 +0200

    MAP
    Lagt inn default kart fra LeafLet som en begynnelse

--- XL-story/script.js
+++ XL-story/script.js
@@ -21,3 +21,15 @@ document.addEventListener('scroll', function () {
 }, {
     passive: true
 });
+
+
+
+/* MAP */
+
+const map = L.map('map').setView([51.505, -0.09], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
