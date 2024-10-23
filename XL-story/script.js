68b774b92650d63895117e7b1c371d37681089f0
Flowerafro <linehenriksen0@gmail.com>
2024-10-23 11:32:44 +0200

    Kart leaflet klart

--- XL-story/script.js
+++ XL-story/script.js
@@ -24,12 +24,52 @@ document.addEventListener('scroll', function () {
 
 
 
-/* MAP */
-
-const map = L.map('map').setView([51.505, -0.09], 10);
+/* LeafLet map: kart */
+const map = L.map('map').setView([59.54181, 10.55744], 10);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+/* Ikoner til markører på kartet */
+/* eks: var greenIcon = L.icon({
+    iconUrl: 'leaf-green.png',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+}); */
+
+
+/* Markører Ulykkested og Ambulanseposisjon  */
+let pinSepsis = L.marker([59.54181, 10.55744]).bindPopup("Sepsispasient på Tofte"),
+    pinHjerteinfarkt = L.marker([59.53612, 10.52807]).bindPopup("Hjerteinfarkt på Tofte "),
+    pinAMKDrammen = L.marker([59.74354, 10.20406]).bindPopup("Ambulanse fra Sætre på oppdrag i Drammen"),
+    pinAMKAas = L.marker([59.629409, 10.605369]).bindPopup("Første ambulanse fra Ås, på vei til sepsis"),
+    pinAMKAas2 = L.marker([59.66519, 10.79308]).bindPopup("Ny ambulanse sendes fra Ås, sendes til hjerteinfarkt");
+
+let pins = L.layerGroup([pinSepsis, pinHjerteinfarkt, pinAMKDrammen, pinAMKAas, pinAMKAas2]);
+pins.addTo(map); // Add pins to the map
+
+let openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
+});
+
+let baseMaps = {
+    "OpenStreetMap": L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }),
+    "OpenTopoMap": openTopoMap
+};
+
+let overlayMaps = {
+    "Pins": pins
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
